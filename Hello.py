d81771ca35289ae4e9184f264f1108fafe7ede8c
Gopalakrishnan <gops1998@gmail.com>
2023-12-01 16:28:15 +0000

    reverting hello.py

--- Hello.py
+++ Hello.py
@@ -1,31 +1,51 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import streamlit as st
-import cv2
-import torch
+from streamlit.logger import get_logger
 
-# Load YOLOv8 model
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = torch.hub.load('yolo-model.pt', 'model', pretrained=True)
-    return model
+LOGGER = get_logger(__name__)
 
-model = load_model()
 
-# Streamlit app
-st.title("YOLOv8 Object Detection")
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    st.write("# Welcome to Streamlit! 👋")
 
-if uploaded_file is not None:
-    image = cv2.imread(uploaded_file.name)[:, :, ::-1]  # BGR to RGB
-    results = model(image)
-    
-    # Display results
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    st.write("Detected Objects:")
-    st.json(results.xyxy[0].numpy().tolist())
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+        **👈 Select a demo from the sidebar** to see some examples
+        of what Streamlit can do!
+        ### Want to learn more?
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+        ### See more complex demos
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
 
 if __name__ == "__main__":
     run()
-
-
-
